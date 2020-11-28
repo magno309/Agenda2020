8d5d2f62fbd662a421644ec7f2b2f5f2857ba479
magno309 <48113838+magno309@users.noreply.github.com>
2020-11-28 13:29:04 -0600

    Creación de actividades para agregar notas y tareas

---
+++ app/src/main/java/com/example/agenda/AgregarTarea.java
@@ -0,0 +1,14 @@
+package com.example.agenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgregarTarea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_tarea);
+    }
+}
--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.agenda;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,8 +10,6 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import com.example.agenda.ui.main.SectionsPagerAdapter;
@@ -26,13 +25,22 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.btnAgregar);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = null;
+                switch (viewPager.getCurrentItem()){
+                    case 0:
+                        intent = new Intent(MainActivity.this, AgregarNota.class);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        intent = new Intent(MainActivity.this, AgregarTarea.class);
+                        startActivity(intent);
+                        break;
+                }
             }
         });
     }

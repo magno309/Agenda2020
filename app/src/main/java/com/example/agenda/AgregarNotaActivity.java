c87fd033885bc23e567c953d3680f1ddf5eeab20
luis-8 <luis_zavala8@outlook.com>
2020-12-08 12:05:21 -0600

    Agregar y guardar imagenes capturadas por la cámara en la memoria externa

--- app/src/main/java/com/example/agenda/AgregarNotaActivity.java
+++ app/src/main/java/com/example/agenda/AgregarNotaActivity.java
@@ -2,14 +2,17 @@ package com.example.agenda;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.Manifest;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.content.pm.PackageManager;
@@ -20,6 +23,7 @@ import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
@@ -29,13 +33,18 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.io.IOException;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class AgregarNotaActivity extends AppCompatActivity {
+
+    private boolean notaGuardada = false;
 
     //Base de datos
     NotasDBHelper dbHelper;
@@ -53,12 +62,13 @@ public class AgregarNotaActivity extends AppCompatActivity {
 
     //Imagenes
     private ImageView imageViewTemp;
-    private List<String> imgsPaths; //imgsDescripciones;
-    private String pathImagenes; // descImagenes, pathImgSeleccionada;
+    private List<String> imgsPaths, imgsCapturadasPaths; //imgsDescripciones;
+    private String pathImagenes, rutaFotoCapturada; // descImagenes, pathImgSeleccionada;
     private AdaptadorImagen adaptadorImagen;
     static final int REQUEST_CODE_STORAGE_PERMISSION = 1;
     static final int REQUEST_CODE_SELECT_IMAGE = 2;
-    static final int REQUEST_IMAGE_CAPTURE = 3;
+    static final int REQUEST_CODE_WRITE_STORAGE_PERMISSION = 3;
+    static final int REQUEST_CODE_IMAGE_CAPTURE = 4;
 
     //MediaRecorder
     private static final String LOG_TAG = "AudioRecordTest";
@@ -85,6 +95,11 @@ public class AgregarNotaActivity extends AppCompatActivity {
                     seleccionarImagen();
                 }
                 break;
+            case REQUEST_CODE_WRITE_STORAGE_PERMISSION:
+                if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    tomarCaptura();
+                }
+                break;
         }
         if (!permissionToRecordAccepted ) finish();
     }
@@ -94,6 +109,39 @@ public class AgregarNotaActivity extends AppCompatActivity {
         if(intent.resolveActivity(getPackageManager()) !=null){
             startActivityForResult(intent, REQUEST_CODE_SELECT_IMAGE);
         }
+    }
+
+    private void tomarCaptura(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = crearArchivoDeImagen();
+            } catch (IOException ex) {
+                Toast.makeText(AgregarNotaActivity.this, "Error al crear el archivo de img: "+ex.getMessage(), Toast.LENGTH_LONG).show();
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(getApplicationContext(),
+                        "com.example.agenda",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_CODE_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private File crearArchivoDeImagen() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        rutaFotoCapturada = image.getAbsolutePath();
+        imgsCapturadasPaths.add(rutaFotoCapturada);
+        return image;
     }
 
     @Override
@@ -127,6 +175,17 @@ public class AgregarNotaActivity extends AppCompatActivity {
                     }
                 }
             }
+        }
+        else if(requestCode == REQUEST_CODE_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            if(rutaFotoCapturada != null && !rutaFotoCapturada.isEmpty()){
+                imgsPaths.add(rutaFotoCapturada);
+
+                adaptadorImagen.notifyItemInserted(imgsPaths.size()-1);
+            }
+            //Log.println(Log.INFO, "rutas", "ruta: "+rutaFotoCapturada);
+            //Uri uri = Uri.fromFile(new File(rutaFotoCapturada));
+            //Log.println(Log.INFO, "rutas", "uri: "+uri.toString());
+
         }
     }
 
@@ -223,6 +282,7 @@ public class AgregarNotaActivity extends AppCompatActivity {
         //pathImgSeleccionada="";
         pathImagenes="";
         imgsPaths=new ArrayList<>();
+        imgsCapturadasPaths=new ArrayList<>();
         //imgsDescripciones=new ArrayList<>();
         dbHelper = new NotasDBHelper(this);
         db = dbHelper.getWritableDatabase();
@@ -244,6 +304,7 @@ public class AgregarNotaActivity extends AppCompatActivity {
             adaptadorImagen = new AdaptadorImagen(this,imgsPaths,null);
             rvMultimedia.setAdapter(adaptadorImagen);
             btnAgregarNota.setOnClickListener(view -> {
+                notaGuardada = true;
                 nombre = txtNombre.getText().toString();
                 descripcion = txtDescrpcion.getText().toString();
                 if(!imgsPaths.isEmpty()){
@@ -283,7 +344,7 @@ public class AgregarNotaActivity extends AppCompatActivity {
                     pathImagenes = c1.getString(c1.getColumnIndex(NotasDB.NotasDatabase.COLUMN_NAME_COL3));
                     //pathImgSeleccionada = c1.getString(c1.getColumnIndex(NotasDB.NotasDatabase.COLUMN_NAME_COL3));
                     //descImagenes = c1.getString(c1.getColumnIndex(NotasDB.NotasDatabase.COLUMN_NAME_COL7));
-                    if(pathImagenes!=null){
+                    if(pathImagenes!=null&&!pathImagenes.isEmpty()){
                        String[] arr = pathImagenes.split(",");
                         for(int i=0; i<arr.length; i++){
                             imgsPaths.add(arr[i]);
@@ -306,6 +367,7 @@ public class AgregarNotaActivity extends AppCompatActivity {
                 btnAgregarNota.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        notaGuardada = true;
                         nombre = txtNombre.getText().toString();
                         descripcion = txtDescrpcion.getText().toString();
                         if(!imgsPaths.isEmpty()){
@@ -344,15 +406,39 @@ public class AgregarNotaActivity extends AppCompatActivity {
         return new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED){
-                    ActivityCompat.requestPermissions(AgregarNotaActivity.this,
-                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE_STORAGE_PERMISSION);
-                }
-                else{
-                    seleccionarImagen();
-
-                }
+                String[] opciones = {"Seleccionar del sistema","Tomar una foto", "Cancelar" };
+                AlertDialog.Builder builder = new AlertDialog.Builder(AgregarNotaActivity.this);
+                builder.setTitle("Selecciona de dónde quieres insertar una imagen");
+                builder.setItems(opciones, new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if(opciones[i].equals("Seleccionar del sistema")){
+                            if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+                                    != PackageManager.PERMISSION_GRANTED){
+                                ActivityCompat.requestPermissions(AgregarNotaActivity.this,
+                                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE_STORAGE_PERMISSION);
+                            }
+                            else{
+                                seleccionarImagen();
+                            }
+                        }
+                        else if(opciones[i].equals("Tomar una foto")){
+                            if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                                    != PackageManager.PERMISSION_GRANTED){
+                                ActivityCompat.requestPermissions(AgregarNotaActivity.this,
+                                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_WRITE_STORAGE_PERMISSION);
+                            }
+                            else{
+                                tomarCaptura();
+                            }
+                        }
+                        else{
+                            dialogInterface.dismiss();
+                        }
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         };
     }
@@ -360,6 +446,11 @@ public class AgregarNotaActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         db.close();
+        if(!notaGuardada){
+            for (String s:imgsCapturadasPaths) {
+                new File(s).delete();
+            }
+        }
         super.onDestroy();
     }
 }
